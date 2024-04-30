5bbdd47dd622d18ba28cb636d42733372921d9f5
Asif Rahman <93546485+asifrahman03@users.noreply.github.com>
2024-04-30 12:36:18 -0400

    Update FreeBlock.java

--- FreeBlock.java
+++ FreeBlock.java
@@ -1,16 +1,35 @@
+// -------------------------------------------------------------------------
+/**
+ * Represents a FreeBlock that has the beginning index to be used in
+ * freeBlocklist
+ * 
+ * @author asifrahman
+ * @version Apr 28, 2024
+ */
 public class FreeBlock {
-    
+
     private int beginningIndex;
-    private int trueSize;
-    
-    public FreeBlock(int start, int tSize) {
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new FreeBlock object.
+     * 
+     * @param start
+     *            beginning index of freeblock
+     */
+    public FreeBlock(int start) {
         this.beginningIndex = start;
-        this.trueSize = tSize;
     }
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
 
-    //~Public  Methods ........................................................
+    // ----------------------------------------------------------
+    /**
+     * gets beginning index
+     * 
+     * @return startIndex
+     */
+    public int getStart() {
+        return this.beginningIndex;
+    }
 
 }
