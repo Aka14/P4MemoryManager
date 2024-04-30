50e1bf4c81abad1ee6942205d3a7e862d8ca48a5
Asif Rahman <93546485+asifrahman03@users.noreply.github.com>
2024-04-30 12:38:26 -0400

    Update Record.java

--- Record.java
+++ Record.java
@@ -1,25 +1,54 @@
+// -------------------------------------------------------------------------
+/**
+ * Record: Holds Handle but for now holds Seminar object with an ID as key
+ * 
+ * @author asifrahman
+ * @version Apr 28, 2024
+ */
 public class Record {
-    
+
     private int id;
-   // private Handle handle;
-    private Seminar sem;
-    
-    public Record(int iD, Seminar s) {
+    private Handle handle;
+
+    // ----------------------------------------------------------
+    /**
+     * Create a new Record object.
+     * 
+     * @param iD
+     *            seminar id
+     * @param s
+     *            seminar object
+     */
+    public Record(int iD, Handle h) {
         this.id = iD;
-        this.sem = s;
+        this.handle = h;
     }
 
+
+    // ----------------------------------------------------------
+    /**
+     * gets ID
+     * 
+     * @return id of seminar
+     */
     public int getId() {
         return id;
     }
 
-//    public Handle getHandle() {
-//        return handle;
-//    }
-    
-    public Seminar getSem() {
-        return sem;
+
+    public Handle getHandle() {
+        return handle;
     }
-    
-    
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets seminar object
+     * 
+     * @return seminar object
+     */
+//    public Seminar getSem() {
+//        return sem;
+//    }
+
 }
